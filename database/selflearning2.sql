7906ff53374e4200c5c2db61fe7e6a6509e3d271
michellepaulinn <102228621+michellepaulinn@users.noreply.github.com>
2022-04-01 23:57:06 +0700

    Update selflearning2.sql

--- database/selflearning2.sql
+++ database/selflearning2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2022 at 05:16 PM
+-- Generation Time: Apr 01, 2022 at 06:56 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -49,7 +49,9 @@ CREATE TABLE `userdata` (
 --
 
 INSERT INTO `userdata` (`nik`, `namaDepan`, `namaTengah`, `namaBelakang`, `tempatLahir`, `tanggalLahir`, `wargaNegara`, `email`, `noHP`, `alamat`, `kodePos`, `profPic`, `username`, `password`) VALUES
-('1234567887654321', 'Michelle', 'Pauline', 'Kusuma', 'Jakarta', '2000-01-28', 'Indonesia', 'michellepaulinn@gmail.com', '081287422012', 'Jalan p.maluku raya c1/6', '17111', 'profPic/2021-10-31 (3).png', 'misell', 'asdf1234');
+('9876543210987654', 'Adam', 'John', 'Carlsen', 'Bandung', '2000-06-21', 'Indonesia', 'adamcarlsen@gmail.com', '081288776212', 'Jalan bunga hitam no 34', '17211', 'profPic/pc2.jpg', 'adamcarl', 'biolab26'),
+('1234567887654321', 'Michelle', 'Pauline', 'Kusuma', 'Jakarta', '2000-01-28', 'Indonesia', 'michellepaulinn@gmail.com', '081287422012', 'Jalan p.maluku raya c1/6', '17111', 'profPic/2021-10-31 (3).png', 'misell', 'asdf1234'),
+('1234567890987654', 'Olive', 'Smith', 'Carlsen', 'Jakarta', '2001-04-26', 'Indonesia', 'Olivesc@gmail.com', '081298533456', 'Jalan Mangga no 5', '17133', 'profPic/pc1.jpg', 'olivesc', 'hypothes1s');
 
 --
 -- Indexes for dumped tables
